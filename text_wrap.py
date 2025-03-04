0f6a91bdf89575ecf9480df38deb5e2cf91a5eaf
Muskan Tariq <155077202+smilingstar78@users.noreply.github.com>
2025-03-04 21:37:41 +0500

    Create text_wrap.py
    Today I wrote code for wrapping text!

---
+++ text_wrap.py
@@ -0,0 +1,10 @@
+import textwrap
+
+def wrap(string, max_width):
+    wrapper = textwrap.wrap(string, max_width)
+    wrapper1 = "\n".join(wrapper)
+    return wrapper1
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
