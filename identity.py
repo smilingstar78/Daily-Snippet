147dd15fde9be989fe500571feed786283889490
Muskan Tariq <155077202+smilingstar78@users.noreply.github.com>
2025-03-08 21:38:18 +0500

    Create identity.py
    My today's snippet for 8th March, 2025 is here........

---
+++ identity.py
@@ -0,0 +1,8 @@
+import numpy
+numpy.set_printoptions(legacy='1.13')
+
+values = list(map(int, input().split()))
+m, n = values
+print(numpy.eye(m, n))
+
+#8th March, 2025, Saturday, 8th Ramadan, 9:36pm
