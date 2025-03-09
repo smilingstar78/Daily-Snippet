9a2eb2f118cd9ec40ce5ec72c8bf67f61a08e08f
Muskan Tariq <155077202+smilingstar78@users.noreply.github.com>
2025-03-09 21:30:33 +0500

    Create numpy_operations.py
    My today's snippet 9th March, 2025 is here.....

---
+++ numpy_operations.py
@@ -0,0 +1,13 @@
+import numpy
+
+numpy.set_printoptions(sign=' ')
+
+arr = list(map(float, input().split()))
+
+my_arr = numpy.array(arr)
+
+print(numpy.floor(my_arr))
+print(numpy.ceil(my_arr))
+print (numpy.rint(my_arr))
+
+#9th March, 2025, Sunday, 9th Ramadan, 9:27pm
