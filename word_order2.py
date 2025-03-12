9728016cc68f4d56b758b6cb87de4f1428a3644f
Muskan Tariq <155077202+smilingstar78@users.noreply.github.com>
2025-03-12 23:55:50 +0500

    Create word_order2.py
    My today's snnipet for 12th March, 2025 is here...

---
+++ word_order2.py
@@ -0,0 +1,16 @@
+from collections import OrderedDict
+
+N = int(input())  # Number of inputs
+word_counts = OrderedDict()  # Maintain order of insertion
+
+for _ in range(N):
+    word = input().strip()
+    word_counts[word] = word_counts.get(word, 0) + 1  # Increment count
+
+# Print number of unique words
+print(len(word_counts))
+
+# Print occurrences in order of appearance
+print(" ".join(map(str, word_counts.values())))
+
+#12th March, 2025, Wednesday, 12th Ramadan, 11:54pm.
