c3789f92412cfbfe05c40071c33abb088586143e
Muskan Tariq <155077202+smilingstar78@users.noreply.github.com>
2025-03-15 22:18:37 +0500

    Create input().py
    My today's snnipet for 15th March , 2025...

---
+++ input().py
@@ -0,0 +1,12 @@
+x, k = list(map(int, input().split()))
+
+P = input()
+
+result = eval(P)
+
+if result == k:
+    print(True)
+else:
+    print(False)
+
+#15th March, 2025, Saturday, 15th Ramadan, 10:18pm
