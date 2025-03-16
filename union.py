e4ce07b29abd57be38d8f70752f589d5768cc4eb
Muskan Tariq <155077202+smilingstar78@users.noreply.github.com>
2025-03-16 21:41:39 +0500

    Create union.py
    My today's snnipet for 16th March, 2025 is here....

---
+++ union.py
@@ -0,0 +1,10 @@
+n = int(input())
+roll = list(map(int, input().split()))
+    
+b = int(input())
+roll2 = list(map(int, input().split()))
+s1 = set(roll)
+s2 = set(roll2)
+print(len(s1|s2))
+
+#16th March, 2025, Sunday, 16th Ramadan, 9:41pm
