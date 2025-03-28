4f32b846a55b9ac519dc06c6d035882069fe8435
Muskan Tariq <155077202+smilingstar78@users.noreply.github.com>
2025-03-28 20:39:39 +0500

    Create polynomials.py

---
+++ polynomials.py
@@ -0,0 +1,8 @@
+import numpy
+
+P = list(map(float, input().split()))
+x = int(input())
+P = numpy.array(P)
+print(numpy.polyval(P, x))
+
+#28th March. 2025, Friday(Jumma tul Wida), 28th Ramadan, 8:39pm
