b1cd9157570ab84d64a74b6434a9069ba4e86e1a
Muskan Tariq <155077202+smilingstar78@users.noreply.github.com>
2025-04-29 23:03:46 +0500

    Create polar_coordinates.py

---
+++ polar_coordinates.py
@@ -0,0 +1,16 @@
+import cmath
+
+# Step 1: Input a complex number
+z = complex(input())
+
+# Step 2: Find modulus
+mod = abs(z)
+
+# Step 3: Find phase
+phase = cmath.phase(z)
+
+# Step 4: Print results
+print(mod)
+print(phase)
+
+#29th April, 2025, Tuesday, 11:03pm...
