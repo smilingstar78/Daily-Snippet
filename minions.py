d6d6b7891178788364bbf45600b2424ac243b8cb
Muskan Tariq <155077202+smilingstar78@users.noreply.github.com>
2025-05-05 23:25:02 +0500

    Create minions.py

---
+++ minions.py
@@ -0,0 +1,25 @@
+def minion_game(string):
+    
+    vowels = "AEIOU"
+    kevin_score = 0
+    stuart_score = 0
+    n = len(string)
+
+    for i in range(n):
+        if string[i] in vowels:
+            kevin_score += n - i
+        else:
+            stuart_score += n - i
+
+    if kevin_score > stuart_score:
+        print("Kevin", kevin_score)
+    elif stuart_score > kevin_score:
+        print("Stuart", stuart_score)
+    else:
+        print("Draw")
+
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
+
+# 5th May, 2025, Tuesday, 11:24pm
